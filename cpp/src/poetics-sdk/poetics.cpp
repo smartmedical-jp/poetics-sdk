8b66589e5a4604438430ee74a7c08a84bb6ba0eb
Taein KIM <taein.kim@poetics-ai.com>
2024-08-22 13:44:28 +0900

    Implement audio_buffer

--- cpp/src/poetics-sdk/poetics.cpp
+++ cpp/src/poetics-sdk/poetics.cpp
@@ -10,15 +10,23 @@ int hello()
 int test_audio_buffer()
 {
     poetics::streaming::audio_buffer::AudioBuffer * audioBuffer = new poetics::streaming::audio_buffer::AudioBuffer();
-    char * data = new char[16000];
+    int bytePerSample = 2;
+    int sampleRate = 8000;
+    int byteSize = bytePerSample * sampleRate;
+    char * data = new char[byteSize];
     // initialize data with 0 to 9
-    for (int i = 0; i < 16000; i++)
+    for (int i = 0; i < byteSize; i++)
     {
         data[i] = i % 10;
     }
 
-    audioBuffer->AppendAudioData(data, 16000);
-    audioBuffer->AppendAudioData(data, 16000);
+    audioBuffer->AppendAudioData(data, byteSize);
+    audioBuffer->AppendAudioData(data, byteSize);
+
+    auto fragment1 = audioBuffer->GetFragmentAt(0, false);
+    auto fragment2 = audioBuffer->GetFragmentAt(1, true);
+
+    audioBuffer->ReleaseAudioDataBefore(1);
 
     return 1;
 }
--- cpp/src/poetics-sdk/streaming/audio_buffer/audio_buffer.hpp
+++ cpp/src/poetics-sdk/streaming/audio_buffer/audio_buffer.hpp
@@ -4,8 +4,13 @@
 #include <mutex>
 #include <utility>
 #include <vector>
+#include <deque>
+#include <memory>
+using std::unique_ptr;
 
 #include "../../poetics_error.h"
+
+#define DEFAULT_BYTE_SIZE 16000
 
 namespace poetics::streaming::audio_buffer {
     class IAudioBuffer {
@@ -13,43 +18,68 @@ namespace poetics::streaming::audio_buffer {
         virtual ~IAudioBuffer() = default;
 
         virtual void AppendAudioData(char* data, size_t data_size) = 0;
-        virtual std::pair<char*, poetics::Error> GetFragmentAt(int fragmentIndex, bool isRecordingFinished) = 0;
+        virtual std::pair<std::vector<char> *, poetics::Error> GetFragmentAt(int fragmentIndex, bool isRecordingFinished) = 0;
         virtual void ReleaseAudioDataBefore(int fragmentIndex) = 0;
     };
 
     class AudioBuffer : public IAudioBuffer {
     public:
-        AudioBuffer(int bytesPerFragment = 16000) 
+        AudioBuffer(int bytesPerFragment = DEFAULT_BYTE_SIZE) 
         {
             _bytesPerFragment = bytesPerFragment;
-            _bytes = new std::vector<char>();
+            _bytes = std::make_unique<std::deque<std::vector<char> *>>();
         }
 
         ~AudioBuffer() {
-            delete _bytes;
         }
 
         void AppendAudioData(char* data, size_t data_size) override {
             mutex.lock();
+            auto fragment = new std::vector<char>;
+            fragment->reserve(_bytesPerFragment);
             if (data_size > 0)
             {
-                for (int i = 0; i < data_size; i++)
-                {
-                    _bytes->push_back(data[i]);
-                }
+                std::copy(data, data + data_size, std::back_inserter(*fragment));
             }
+            _bytes->push_back(fragment);
             mutex.unlock();
         }
-        std::pair<char*, poetics::Error> GetFragmentAt(int fragmentIndex, bool isRecordingFinished) override {
-            return std::pair<char *, poetics::Error>();
+
+        std::pair<std::vector<char> *, poetics::Error> GetFragmentAt(int fragmentIndex, bool isRecordingFinished) override {
+            mutex.lock();
+
+            std::vector<char> * fragment;
+            if (fragmentIndex < _fromFragmentIndex) {
+                return std::pair(nullptr, poetics::Error(fmt::format("fragment {} is already released", fragmentIndex)));
+            }
+
+            if (fragmentIndex > _bytes->size() - 1) {
+                return std::pair(nullptr, poetics::Error(fmt::format("fragment {} is not found", fragmentIndex)));
+            }
+
+            fragment = _bytes->at(fragmentIndex);
+
+            mutex.unlock();
+            return std::pair<std::vector<char> *, poetics::Error>(fragment, poetics::Error());
         }
+        
         void ReleaseAudioDataBefore(int fragmentIndex) override {
-
+            mutex.lock();
+            while (_bytes->size() > 0 && _bytes->front()->size() == 0) {
+                delete _bytes->front();
+                _bytes->pop_front();
+            }
+            while (_bytes->size() > fragmentIndex) {
+                delete _bytes->front();
+                _bytes->pop_front();
+            }
+            _fromFragmentIndex = fragmentIndex;
+            mutex.unlock();
         }
 
-        std::vector<char> * GetBytes() { return _bytes; }
+        std::deque<std::vector<char> *> * GetBytes() { return _bytes.get(); }
     private:
-        std::vector<char> * _bytes;
+        unique_ptr<std::deque<std::vector<char> *>> _bytes;
         int _fromFragmentIndex = 0;
         int _bytesPerFragment;
         int _discardableFragmentIndex = 0;
