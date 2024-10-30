851918cf384b5fb8e1c1ae60f4fcd279a145f926
Taein KIM <sappho192@gmail.com>
2024-10-30 15:38:08 +0900

    Make example code to support C++14

--- cpp/src/poetics_example/main.cpp
+++ cpp/src/poetics_example/main.cpp
@@ -1,29 +1,37 @@
 // main.cpp
 #include "main.h"
 
-class StreamListener : public poetics::streaming::asr_job::StreamAsrJobListener
-{
-public:
-    void OnMessageReceived(const string& result) override
-    {
-        cout << "\033[1mOnMessage: " << result << "\033[0m" << endl;
-    }
+namespace poetics {
+    namespace streaming {
+        namespace asr_job {
+            class StreamListener : public StreamAsrJobListener
+            {
+            public:
+                void OnMessageReceived(const string& result) override
+                {
+                    cout << "\033[1mOnMessage: " << result << "\033[0m" << endl;
+                }
 
-    void OnErrorReceived(const string& error) override
-    {
-        cout << "\033[1;31mOnError: " << error << "\033[0m" << endl;
-    }
+                void OnErrorReceived(const string& error) override
+                {
+                    cout << "\033[1;31mOnError: " << error << "\033[0m" << endl;
+                }
 
-    void OnDebugMessageReceived(const string& message) override
-    {
-        cout << "\033[1;33mOnDebug: " << message.substr(0, 250) << "\033[0m" << endl;
+                void OnDebugMessageReceived(const string& message) override
+                {
+                    cout << "\033[1;33mOnDebug: " << message.substr(0, 250) << "\033[0m" << endl;
+                }
+            };
+        }
     }
-};
+}
 
 int main()
 {
     system("chcp 65001");
 
+    // POETICS_SETENV("POETICS_API_KEY", "YOUR_API_KEY");
+    // POETICS_SETENV("POETICS_STREAMING_API_ENDPOINT", "stream.api.poetics-ai.com");
 
     const char* apiKey = getenv("POETICS_API_KEY");
     const char* endpoint = getenv("POETICS_STREAMING_API_ENDPOINT");
@@ -43,15 +51,15 @@ int main()
     poetics::streaming::StreamingClient streamingClient(endpoint, apiKey);
     vector<const char *> channels = { "channel1" };
     unique_ptr<StreamAsrJob> asrJob = streamingClient.createStreamAsrJob("s16le", 8000, 1, true, "test", channels);
-    asrJob->setListener(std::make_shared<StreamListener>());
+    asrJob->setListener(std::make_shared<poetics::streaming::asr_job::StreamListener>());
     if (asrJob->connect())
     {
         cout << "Connected" << endl;
 
         string filePath = "C:\\REPO\\poetics-sdk\\testdata\\short_s16le_8k.wav";
         // string filePath = "C:\\REPO\\poetics-sdk\\testdata\\long_s16le_8k.wav";
-        auto sampleRate = 8000;
-        auto bytesPerSample = 2;
+        int sampleRate = 8000;
+        int bytesPerSample = 2;
         double recordingBufferMilliseconds = 500.0;
         int recordingBufferSize = sampleRate * bytesPerSample * recordingBufferMilliseconds / 1000;
         const int CHANNEL = 0;
@@ -70,8 +78,6 @@ int main()
             {
                 // Sleep for recordingBufferMilliseconds 
                 std::this_thread::sleep_for(std::chrono::milliseconds((int) recordingBufferMilliseconds));
-                // std::this_thread::sleep_for(std::chrono::milliseconds(((int) recordingBufferMilliseconds) - 100));
-                // std::this_thread::sleep_for(std::chrono::milliseconds(150));
 
                 audioFile.read(buffer, recordingBufferSize);
 
@@ -97,13 +103,13 @@ int main()
                 
                 index++;
             }
+
         }
         else {
             cout << "Failed to open audio file" << endl;
         }
         delete[] buffer;
         buffer = nullptr;
-
         cout << "Closing StreamAsrJob" << endl;
         asrJob->finishEnqueuingAudioData();
         // Wait for 10 seconds before disconnecting
